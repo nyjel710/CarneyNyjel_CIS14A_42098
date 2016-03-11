bc6c8bfa54c83ea26ec903f9c7a7d842ae9dbf94
nyjel710 <User@Nyjels-Mac.local>
2016-03-10 18:08:30 -0800

    did lab 1

---
+++ Labs/Lab 1/code2.js
@@ -0,0 +1,12 @@
+// Test for jokes
+var joke = "JavaScript walked into a bar";
+var toldJoke = false;
+var punchline = "Better watch out for those semi-colons";
+var ventage = 20;
+var result;
+
+if (toldJoke == true) {
+	alert(punchline);
+} else {
+	alert(joke);
+}
